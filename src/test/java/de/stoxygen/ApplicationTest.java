654301448588c4f8e97f0ad3e31c73461ba5003c
Raphael Lekies <hack3d.dev@googlemail.com>
2018-07-03 01:54:15 +0200

    Delete hardcoded profiles in test classes.

--- src/test/java/de/stoxygen/ApplicationTest.java
+++ src/test/java/de/stoxygen/ApplicationTest.java
@@ -3,7 +3,6 @@ package de.stoxygen;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ApplicationTest {
 
     @Test
-    @Profile("test")
     public void contextLoads() throws Exception {
 
     }
--- src/test/java/de/stoxygen/BitstampSymbolsTest.java
+++ src/test/java/de/stoxygen/BitstampSymbolsTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -13,7 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
 public class BitstampSymbolsTest {
 
     @Autowired
